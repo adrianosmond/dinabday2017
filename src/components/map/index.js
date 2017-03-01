80307a291468cecee9c0eb0b6cf4ab6677c060a7
Adrian Osmond <a.c.osmond@gmail.com>
2017-03-01 21:00:13 +0000

    More terrain types. Player avatar

--- src/components/map/index.js
+++ src/components/map/index.js
@@ -7,7 +7,8 @@ export default class Map extends Component {
 			rows: []
 		},
 		currentX: 11,
-		currentY: 2
+		currentY: 2,
+		moving: false
 	};
 
 
@@ -26,23 +27,40 @@ export default class Map extends Component {
 
 	moveMap(e) {
 		let direction = e.detail;
+		let moved = false;
 
 		if (direction === "north" && this.canGoNorth()) {
+			moved = true;
 			this.setState({
 				currentY: this.state.currentY - 1
 			});
 		} else if (direction === "south" && this.canGoSouth()) {
+			moved = true;
 			this.setState({
 				currentY: this.state.currentY + 1
 			});
 		} else if (direction === "east" && this.canGoEast()) {
+			moved = true;
 			this.setState({
 				currentX: this.state.currentX + 1
 			});
 		} else if (direction === "west" && this.canGoWest()) {
+			moved = true;
 			this.setState({
 				currentX: this.state.currentX - 1
 			});
+		}
+
+		if (moved) {
+			this.setState({
+				moving: true
+			});
+
+			setTimeout(() => {
+				this.setState({
+					moving: false
+				});				
+			}, 500);
 		}
 	}
 
@@ -54,18 +72,15 @@ export default class Map extends Component {
 				|| potentialNeighbour - 2 === current;
 	}
 
-	cellOpacity(rowIdx, colIdx) {
-		var str = "opacity: ";
-		if (rowIdx === this.state.currentY && colIdx === this.state.currentX) {
-			str += 1;
-		} else if (this.neighbouring(rowIdx, this.state.currentY) && this.neighbouring(colIdx, this.state.currentX)) {
-			str += 0.6;
-		} else {
-			str += 0;
-		}
-		str += ";";
-		return str;
+	cellVisible(rowIdx, colIdx) {
+		return this.neighbouring(rowIdx, this.state.currentY) && 
+			this.neighbouring(colIdx, this.state.currentX);
 	}
+
+	// cellClick(rowIdx, colIdx, cell) {
+	// 	var newHeight = (cell.height + 1) % 4;
+	// 	firebase.database().ref("map/rows/" + rowIdx + "/cols/" + colIdx + "/height").set(newHeight);
+	// }
 
 	mapTransform() {
 		var xTransform = ((10.5 - this.state.currentX) * 30) + "px";
@@ -74,24 +89,28 @@ export default class Map extends Component {
 		return "transform: rotateX(55deg) scale(3) translateX(" + xTransform + ") translateY(" + yTransform + ");"
 	}
 
+	terrainPassable(height) {
+		return height > 0 && height < 3;
+	}
+
 	canGoNorth() {
 		if (this.state.map.rows.length === 0) return false;
-		return this.state.map.rows[this.state.currentY - 1].cols[this.state.currentX].height > 0;
+		return this.terrainPassable(this.state.map.rows[this.state.currentY - 1].cols[this.state.currentX].height);
 	}
 
 	canGoSouth() {
 		if (this.state.map.rows.length === 0) return false;
-		return this.state.map.rows[this.state.currentY + 1].cols[this.state.currentX].height > 0;
+		return this.terrainPassable(this.state.map.rows[this.state.currentY + 1].cols[this.state.currentX].height);
 	}
 
 	canGoEast() {
 		if (this.state.map.rows.length === 0) return false;
-		return this.state.map.rows[this.state.currentY].cols[this.state.currentX + 1].height > 0;
+		return this.terrainPassable(this.state.map.rows[this.state.currentY].cols[this.state.currentX + 1].height);
 	}
 
 	canGoWest() {
 		if (this.state.map.rows.length === 0) return false;
-		return this.state.map.rows[this.state.currentY].cols[this.state.currentX - 1].height > 0;
+		return this.terrainPassable(this.state.map.rows[this.state.currentY].cols[this.state.currentX - 1].height);
 	}
 
 	render() {
@@ -102,14 +121,14 @@ export default class Map extends Component {
 					east={this.canGoEast()}
 					west={this.canGoWest()} />
 				<div class="map__fog"></div>
+				<div class={"map__avatar" + (this.state.moving? ' map__avatar--walking' : '')}></div>
 				<div class="map__inner" style={this.mapTransform()}>
 					{this.state.map.rows.map((row, rowIdx) => {
 						return (
 							<div class="map__row">
 							{row.cols.map((cell, colIdx) => {
 								return (
-									<div class={'map__cell map__cell--height-' + cell.height}
-										style={this.cellOpacity(rowIdx, colIdx)}></div>
+									<div class={'map__cell map__cell--height-' + cell.height + (!this.cellVisible(rowIdx, colIdx) ? ' map__cell--hidden': '') }></div>
 								);
 							})}
 							</div>
